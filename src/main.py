8d5f27e1c718722a1add292e6646ae3824eed9bf
Laiyuhung <laiyuhung921118@gmail.com>
2024-11-23 23:18:18 +0800

    1123 night

--- src/main.py
+++ src/main.py
@@ -58,10 +58,10 @@ def main():
 
     # hopes
     seq_type = ["LSTM", "Transformer", "SimpleRNN","GRU",  "BidirectionalLSTM"]
-    reg_type = ["KNN", "Voting", "Linear", "RandomForest", "GradientBoosting", "SupportVector", "XGBoost", "CatBoost", "LightGBM", "ElasticNet"]
+    reg_type = ["RandomForest", "GradientBoosting", "SupportVector", "XGBoost", "CatBoost", "LightGBM", "ElasticNet"]
     batch_size_option = [256, 128]
     epoch_option = [50, 100, 150, 200, 250, 300]
-    start_k = 23
+    start_k = 60
     # seq_type = ["Transformer"]
     # reg_type = ["Linear", "GradientBoosting"]
     # batch_size_option = [256]
